5f330c5d9a9b8a05a3e7acce6dedfa1297278247
Shenya De Silva <shenya44@outlook.com>
2023-09-03 01:37:05 +0200

    Included rendering

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import graphviz
 import pandas as pd
 
-
 def main():
 	#Open and load csvs 
 	df = pd.read_csv('test1.csv')
@@ -10,6 +9,8 @@ def main():
 	#Create the Nodes for columns  
 	for i in df.columns:
 		dot.node(i)
-	print(dot.source) 
+	print(dot.source)
+	dot.render('./test.gv')
+	dot.render('./test.gv', view=True)
 if __name__ == "__main__":
     main()
