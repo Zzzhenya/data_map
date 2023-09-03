420e6c317a8d603e3d29f470c92b2998ec0ff0ef
Shenya De Silva <shenya44@outlook.com>
2023-09-03 17:19:58 +0200

    changed engine to fdp

--- test.py
+++ test.py
@@ -18,16 +18,27 @@ def create_graph(dot, df):
 		dot.edge(i, i + str(df.dtypes[i]))
 		dot.node(i + "count" + str(df[i].count()), "Count: "+ str(df[i].count()))
 		dot.edge(i, i + "count" + str(df[i].count()))
-		dot.node(i + "min" + str(df[i].min()), "Min: "+ str(df[i].min()))
-		dot.edge(i, i + "min" + str(df[i].min()))
-		dot.node(i + "max" + str(df[i].max()), "Max: "+ str(df[i].max()))
-		dot.edge(i, i + "max" + str(df[i].max()))
+		if (str(df.dtypes[i]) == "object"):
+			dot.node(i + "Categorical", "Categorical")
+			dot.edge(i, i + "Categorical")
+		else:
+			dot.node(i + "Numeric", "Numeric")
+			dot.edge(i, i + "Numeric")
+			dot.node(i + "min" + str(df[i].min()), "Min: "+ str(df[i].min()))
+			dot.edge(i, i + "min" + str(df[i].min()))
+			dot.node(i + "max" + str(df[i].max()), "Max: "+ str(df[i].max()))
+			dot.edge(i, i + "max" + str(df[i].max()))
+		
+		dot.node(i + "nunique" + str(df[i].nunique()), "nunique: "+ str(df[i].nunique()))
+		dot.edge(i, i + "nunique" + str(df[i].nunique()))
+		dot.node(i + "nullcount" + str(df[i].isna().sum()), "nullcount: "+ str(df[i].isna().sum()))
+		dot.edge(i, i + "nullcount" + str(df[i].isna().sum()))
 
 def main():
 	#Open and load csvs 
-	df = pd.read_csv('test1.csv')
+	df = pd.read_csv('../Supermarket_data/supermarket_sales - Sheet1.csv')
 	#Initiate the graph
-	dot = graphviz.Graph(engine = "circo")
+	dot = graphviz.Graph(engine = "fdp")
 	create_graph(dot, df)
 	draw_graph(dot)
 
