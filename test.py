fa515b1d9a97520ad78db5a63e843fb7d8df1a56
Shenya De Silva <shenya44@outlook.com>
2023-09-03 16:14:06 +0200

    Included numbering for nodes

--- test.py
+++ test.py
@@ -1,22 +1,31 @@
 import graphviz
 import pandas as pd
 
-def main():
-	#Open and load csvs 
-	df = pd.read_csv('test1.csv')
-	#Initiate the graph
-	dot = graphviz.Digraph()
-	dot.node("Graph")
-	dot.attr('node', shape='box')
+def draw_graph(dot):
+	#print(dot.source)
+	dot.render('./test.gv', view=True)
+	#dot.render('./test.gv')
+
+
+def create_graph(dot, df):
+	a = 1
+	dot.attr('node', shape='box', style = "rounded")
+	dot.node('0', "Graph")
 	#Create the Nodes for columns  
 	for i in df.columns:
 		dot.node(i)
-		dot.edge("Graph", i)
-		dot.node(str(df.dtypes[i]))
-		dot.edge(i, str(df[i].dtype))
+		dot.edge('0', i)
+		dot.node(str(a), str(df.dtypes[i]))
+		dot.edge(i, str(a))
+		a = a + 1
 
-	print(dot.source)
-	dot.render('./test.gv')
-	dot.render('./test.gv', view=True)
+def main():
+	#Open and load csvs 
+	df = pd.read_csv('test1.csv'
+	#Initiate the graph
+	dot = graphviz.Graph()
+	create_graph(dot, df)
+	draw_graph(dot)
+
 if __name__ == "__main__":
     main()
