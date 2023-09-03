846c3e605dacda65b79b4ce0b7a69a88351f3ea6
Shenya De Silva <shenya44@outlook.com>
2023-09-03 02:11:23 +0200

    Included dtype attributes

--- test.py
+++ test.py
@@ -5,10 +5,16 @@ def main():
 	#Open and load csvs 
 	df = pd.read_csv('test1.csv')
 	#Initiate the graph
-	dot = graphviz.Graph()
+	dot = graphviz.Digraph()
+	dot.node("Graph")
+	dot.attr('node', shape='box')
 	#Create the Nodes for columns  
 	for i in df.columns:
 		dot.node(i)
+		dot.edge("Graph", i)
+		dot.node(str(df.dtypes[i]))
+		dot.edge(i, str(df[i].dtype))
+
 	print(dot.source)
 	dot.render('./test.gv')
 	dot.render('./test.gv', view=True)
