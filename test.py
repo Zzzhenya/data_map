671ab4cb63654cec9c30f8278089f5592529075e
Shenya De Silva <shenya44@outlook.com>
2023-09-03 16:47:20 +0200

    made node names unique

--- test.py
+++ test.py
@@ -9,21 +9,25 @@ def draw_graph(dot):
 
 def create_graph(dot, df):
 	dot.attr('node', shape='box', style = "rounded")
-	dot.node('0', "Graph")
+	dot.node('0', "DataFrame")
 	#Create the Nodes for columns  
 	for i in df.columns:
 		dot.node(i)
 		dot.edge('0', i)
 		dot.node(i + str(df.dtypes[i]), "Dtype: "+ str(df.dtypes[i]))
 		dot.edge(i, i + str(df.dtypes[i]))
-		dot.node(i+str(df[i].count()), "Count: "+ str(df[i].count()))
-		dot.edge(i, i+str(df[i].count()))
+		dot.node(i + "count" + str(df[i].count()), "Count: "+ str(df[i].count()))
+		dot.edge(i, i + "count" + str(df[i].count()))
+		dot.node(i + "min" + str(df[i].min()), "Min: "+ str(df[i].min()))
+		dot.edge(i, i + "min" + str(df[i].min()))
+		dot.node(i + "max" + str(df[i].max()), "Max: "+ str(df[i].max()))
+		dot.edge(i, i + "max" + str(df[i].max()))
 
 def main():
 	#Open and load csvs 
 	df = pd.read_csv('test1.csv')
 	#Initiate the graph
-	dot = graphviz.Graph()
+	dot = graphviz.Graph(engine = "circo")
 	create_graph(dot, df)
 	draw_graph(dot)
 
